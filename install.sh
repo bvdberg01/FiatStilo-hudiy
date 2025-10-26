5894b027181b03fc73d9f09ebbd454ea54ad5e63
bvdberg01 <74251551+bvdberg01@users.noreply.github.com>
2025-10-26 16:25:45 +0100

    Update install script with Python venv and npm serve
    Adds installation of the global 'serve' npm package and sets up a Python virtual environment in the Scripts directory. Also installs 'protobuf' and 'websocket-client' Python packages to support required dependencies.

--- install.sh
+++ install.sh
@@ -2,8 +2,15 @@
 
 sudo apt update
 sudo apt install -y npm
+sudo npm install -g serve
 
 SERVICE_DIR="/home/stilo/FiatStilo-hudiy/Services"
+SCRIPTS_DIR="/home/stilo/FiatStilo-hudiy/Scripts"
+
+cd "$SCRIPTS_DIR"
+python3 -m venv venv
+source venv/bin/activate
+pip install protobuf websocket-client
 
 for service in "$SERVICE_DIR"/*.service; do
     if [ -f "$service" ]; then
